43f6c22272033f27e841e9314ac184f5cee8bf40
Vladimir Borisov <vladimir.borisov@x3me.net>
2018-03-14 17:49:59 +0200

    Initial scan for links

--- add-on/main.js
+++ add-on/main.js
@@ -1,7 +1,10 @@
+var linkPattern = 'a[href^="https://slack-redir.net/link?url="]';
 var element = document.getElementById('col_messages');
 
-function fixLink(link) {
-	link.href = decodeURIComponent(link.href.substr(link.href.indexOf('url=')+4).split('&').pop());
+function fixLinks(links) {
+	for(let link of links) {
+		link.href = decodeURIComponent(link.href.substr(link.href.indexOf('url=')+4).split('&').pop());
+	}
 }
 
 var observer = new MutationObserver(function(mList) {
@@ -9,9 +12,9 @@ var observer = new MutationObserver(function(mList) {
 		if(mut.type !== "childList" || mut.addedNodes.length === 0 ) {
 			continue;
 		}
-		for(let link of mut.target.querySelectorAll('a[href^="https://slack-redir.net/link?url="]')) {
-			fixLink(link);
-		}
+		fixLinks(mut.target.querySelectorAll(linkPattern));
 	}
 });
 observer.observe(element, {childList: true, subtree: true });
+
+fixLinks(element.querySelectorAll(linkPattern));

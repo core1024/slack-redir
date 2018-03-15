94528919d090b6110df80b933b1f532a68897acb
Vladimir Borisov <vladimir.borisov@x3me.net>
2018-03-15 11:06:17 +0200

    Show chain icon at the end of firect links. Bug fixes

--- add-on/main.js
+++ add-on/main.js
@@ -1,20 +1,29 @@
 var linkPattern = 'a[href^="https://slack-redir.net/link?url="]';
 var element = document.getElementById('col_messages');
 
-function fixLinks(links) {
-	for(let link of links) {
-		link.href = decodeURIComponent(link.href.substr(link.href.indexOf('url=')+4).split('&').pop());
-	}
+function fixLink(link) {
+	link.href = decodeURIComponent(link.href.substr(link.href.indexOf('url=')+4).split('&').pop());
+	link.innerText += " \uD83D\uDD17";
+	link.removeAttribute('onclick');
+	link.parentNode.replaceChild(link.cloneNode(true), link);
 }
 
 var observer = new MutationObserver(function(mList) {
 	for(let mut of mList) {
-		if(mut.type !== "childList" || mut.addedNodes.length === 0 ) {
-			continue;
+		if(mut.type === "attributes" && mut.target.href.substr(0, 33) === "https://slack-redir.net/link?url=") {
+			fixLink(mut.target);
 		}
-		fixLinks(mut.target.querySelectorAll(linkPattern));
+		if(mut.type === "childList" && mut.addedNodes.length > 0 ) {
+			mut.target.querySelectorAll(linkPattern).forEach(fixLink);
+		}
 	}
 });
-observer.observe(element, {childList: true, subtree: true });
+observer.observe(element, {
+	childList: true,
+	subtree: true,
+	attributes: true,
+	attributeFilter: ['href']
+});
 
-fixLinks(element.querySelectorAll(linkPattern));
+element.querySelectorAll(linkPattern).forEach(fixLink);
+
